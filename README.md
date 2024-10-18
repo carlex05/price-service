94dc7dccb82189949ae5a1d92e7ec7242ff25c87
Carlos Gómez <cgomezr@riu.com>
2024-10-18 17:56:21 +0200

    Adjusting base project to generate correctly the open api generator code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pricing-service
+
+http://localhost:8080/swagger-ui/index.html
--- src/main/java/com/inditex/priceservice/infrastructure/controller/PriceController.java
+++ src/main/java/com/inditex/priceservice/infrastructure/controller/PriceController.java
@@ -1,11 +1,22 @@
 package com.inditex.priceservice.infrastructure.controller;
 
 import com.inditex.priceservice.infrastructure.generated.api.DefaultApi;
+import com.inditex.priceservice.infrastructure.generated.model.PriceResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
 
 /**
  *
  * @author cgomezr
  */
+@RestController
 public class PriceController implements DefaultApi {
-    
+
+
+    @Override
+    public ResponseEntity<PriceResponseDto> getPrices(OffsetDateTime applicationDate, Integer productId, Integer brandId) {
+        return null;
+    }
 }
